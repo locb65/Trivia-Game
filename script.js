07775d7482dddd1d0a503a34c09d06bba12b883a
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-01-03 12:45:18 -0500

    added and object for questions to JS and adjusted classes on html

---
+++ script.js
@@ -0,0 +1,10 @@
+const questions = [{
+    img: 'https://www.google.com/search?q=HE+periodic+table&tbm=isch&ved=2ahUKEwj-nsWe-Kv8AhVpGFkFHfANA28Q2-cCegQIABAA&oq=HE+periodic+table&gs_lcp=CgNpbWcQAzIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMggIABAFEAcQHjIICAAQBRAHEB46BAgjECdQpgtY0xFg4xVoAHAAeACAAUaIAfcBkgEBNJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=u2e0Y77VEOmw5NoP8JuM-AY&bih=890&biw=981#imgrc=8dDkQQXD8ljjCM',
+    q: 'What Element is this?',
+    a: [
+        { choice: 'Hydrogen', isCorrect: false},
+        { choice: 'Helium', isCorrect: true},
+        { choice: 'Phosporus', isCorrect: true},
+        { choice: 'Sodium', isCorrect: true}
+    ]
+}]

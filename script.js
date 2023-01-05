6687224346db9aad3a71e75e682afb8ef4cf02ad
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-01-05 13:23:00 -0500

    functions iterates first question with some bugs

--- script.js
+++ script.js
@@ -6,54 +6,55 @@ const questions = [{
     answers: [
         { choice: 'Hydrogen', isCorrect: false},
         { choice: 'Helium', isCorrect: true},
-        { choice: 'Phosporus', isCorrect: true},
-        { choice: 'Sodium', isCorrect: true}
+        { choice: 'Phosporus', isCorrect: false},
+        { choice: 'Sodium', isCorrect: false}
     ]
 }]
 
 
-//relevant constants and DOM elements needed for displaying questions and choices
-const startButton = document.querySelector('.start')
-const question = document.querySelector('.js-questions');
-const questionImage = document.querySelector('.questionImage');
-const choice1 = document.querySelector('.js-choiceOne');
-const choice2 = document.querySelector('.js-choiceTwo');
-const choice3 = document.querySelector('.js-choiceThree');
-const choice4 = document.querySelector('.js-choiceFour')
+start = true
+console.log('helloworld')
 
+
+//relevant constants and DOM elements needed for displaying questions and choices
+const startButton = document.querySelector('.js-start')
 
 
 //basic function for selecting questions from the questions object
 function questionSelector(questionNumber) {
 
+    const question = document.querySelector('.js-questions');
+    const questionImage = document.querySelector('.questionImage');
+    const choice1 = document.querySelector('.js-choiceOne');
+    const choice2 = document.querySelector('.js-choiceTwo');
+    const choice3 = document.querySelector('.js-choiceThree');
+    const choice4 = document.querySelector('.js-choiceFour');
+
+console.log('helloworld')
+
 //sets the image associated with each question
-questionImage.innerHTML = question[questionNumber].questionImage
+// questionImage.innerText = question[questionNumber].questionImage;
 
 //prints the question by its question number attribute.
 question.innerText = questions[questionNumber].questionText;
 
 //sets the answers to option buttons in HTML
-choice1.innerText = questions[questionNumber].a[0].choice
-choice1.innerText = questions[questionNumber].a[1].choice
-choice1.innerText = questions[questionNumber].a[2].choice
-choice1.innerText = questions[questionNumber].a[3].choice
+choice1.innerText = questions[questionNumber].answers[0].choice;
+choice2.innerText = questions[questionNumber].answers[1].choice;
+choice3.innerText = questions[questionNumber].answers[2].choice;
+choice4.innerText = questions[questionNumber].answers[3].choice;
 
 // sets an incorrect or correct value to each answer for the question.
-choice1.value = question[questionNumber].a[0].isCorrect
-choice1.value = question[questionNumber].a[1].isCorrect
-choice1.value = question[questionNumber].a[2].isCorrect
-choice1.value = question[questionNumber].a[3].isCorrect
+choice1.value = question[questionNumber].answers[0].isCorrect;
+choice2.value = question[questionNumber].answers[1].isCorrect;
+choice3.value = question[questionNumber].answers[2].isCorrect;
+choice4.value = question[questionNumber].answers[3].isCorrect;
 
-if (startButton) {
-    questionSelector('0');
-}
 }
 
+
+//DOM event for start button to iterate first question.
 startButton.addEventListener('click', () => {
-    if(questionNumber < 1) {
-        questionNumber++;
-        questionSelector(questionNumber);
-        console.log(questionNumber)
-    }
 
+    questionSelector(0)
 })

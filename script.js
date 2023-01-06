840ac0f60a3232ec61a621cec73408fc1d957f24
locb65 <locb65@gmail.com>
2023-01-05 19:09:11 -0500

    function first question

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Object of bank of questions and answers to pull from.
 const questions = [{
     questionNumber: 0,
-    img: 'https://www.google.com/search?q=HE+periodic+table&tbm=isch&ved=2ahUKEwj-nsWe-Kv8AhVpGFkFHfANA28Q2-cCegQIABAA&oq=HE+periodic+table&gs_lcp=CgNpbWcQAzIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMggIABAFEAcQHjIICAAQBRAHEB46BAgjECdQpgtY0xFg4xVoAHAAeACAAUaIAfcBkgEBNJgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=u2e0Y77VEOmw5NoP8JuM-AY&bih=890&biw=981#imgrc=8dDkQQXD8ljjCM',
+    img: 'https://www.mathtutordvd.com/members/images/3639.gif',
     questionText: 'What Element is this?',
     answers: [
         { choice: 'Hydrogen', isCorrect: false},
@@ -10,11 +10,7 @@ const questions = [{
         { choice: 'Sodium', isCorrect: false}
     ]
 }]
-
-
-start = true
-console.log('helloworld')
-
+questionNumber = 0
 
 //relevant constants and DOM elements needed for displaying questions and choices
 const startButton = document.querySelector('.js-start')
@@ -24,16 +20,18 @@ const startButton = document.querySelector('.js-start')
 function questionSelector(questionNumber) {
 
     const question = document.querySelector('.js-questions');
-    const questionImage = document.querySelector('.questionImage');
+    const questionImage = document.getElementById('js-image');
     const choice1 = document.querySelector('.js-choiceOne');
     const choice2 = document.querySelector('.js-choiceTwo');
     const choice3 = document.querySelector('.js-choiceThree');
     const choice4 = document.querySelector('.js-choiceFour');
 
+    console.log(questionImage)
+
 console.log('helloworld')
 
 //sets the image associated with each question
-// questionImage.innerText = question[questionNumber].questionImage;
+questionImage.src = questions[questionNumber].img;
 
 //prints the question by its question number attribute.
 question.innerText = questions[questionNumber].questionText;
@@ -45,10 +43,10 @@ choice3.innerText = questions[questionNumber].answers[2].choice;
 choice4.innerText = questions[questionNumber].answers[3].choice;
 
 // sets an incorrect or correct value to each answer for the question.
-choice1.value = question[questionNumber].answers[0].isCorrect;
-choice2.value = question[questionNumber].answers[1].isCorrect;
-choice3.value = question[questionNumber].answers[2].isCorrect;
-choice4.value = question[questionNumber].answers[3].isCorrect;
+choice1.value = questions[questionNumber].answers[0].isCorrect;
+choice2.value = questions[questionNumber].answers[1].isCorrect;
+choice3.value = questions[questionNumber].answers[2].isCorrect;
+choice4.value = questions[questionNumber].answers[3].isCorrect;
 
 }
 
@@ -57,4 +55,7 @@ choice4.value = question[questionNumber].answers[3].isCorrect;
 startButton.addEventListener('click', () => {
 
     questionSelector(0)
+    questionNumber++
+    startButton.style.display = 'none'
 })
+
